19b9ab0e9e158c46840a6132b050562a092c7b07
koto-ista <kaito.koto@i-standard.jp>
2026-03-09 22:29:59 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# qiita_handson
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/qiita_handson/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qiita_handson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
