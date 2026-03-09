a8c37c80d126dd4711408510eedddcaa8077c404
koto-ista <kaito.koto@i-standard.jp>
2026-03-09 22:54:17 +0900

    初期画面の修正

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Qiita Search', // titleを追加
+      theme: ThemeData( // themeを追加
+        primarySwatch: Colors.green,
+        fontFamily: 'Hiragino Sans',
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF55C500),
+        ),
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: const Colors.white,
         ),
       ),
+      home: const SearchScreen(), // SearchScreenを設定
     );
   }
 }
